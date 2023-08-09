114a46fbea60809168a14f8f64e3f11aaa80ed9b
Priyanshu Gupta <priyanshugupta7967@gmail.com>
2023-08-09 11:01:32 +0530

    Update dbConnect.js

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 module.exports= async ()=>{
-    const mongoUri = "mongodb+srv://cluster0.ikwho1s.mongodb.net/?retryWrites=true&w=majority";
+    const mongoUri = "mongodb+srv://priyanshugupta7967:work%405338047@cluster0.ikwho1s.mongodb.net/?retryWrites=true&w=majority";
 
     try{
         const connect=await mongoose.connect(mongoUri);
